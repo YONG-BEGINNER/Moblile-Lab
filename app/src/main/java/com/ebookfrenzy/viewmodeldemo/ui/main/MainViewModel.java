bab65a4c75c766f5e55305b72f279e8d6b0477b6
YONG-BEGINNER <1191200358@student.mmu.edu.my>
2021-01-12 16:49:25 +0800

    Lab 6.1(USD to MYR)

--- app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainViewModel.java
+++ app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainViewModel.java
@@ -3,13 +3,13 @@ package com.ebookfrenzy.viewmodeldemo.ui.main;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    private static final Float usd_to_eu_rate = 0.74F;
+    private static final Float usd_to_myr_rate = 4.06F;
     private String dollarText = "";
     private Float result = 0F;
 
     public void setAmount(String value) {
         this.dollarText = value;
-        result = Float.parseFloat(dollarText)*usd_to_eu_rate;
+        result = Float.parseFloat(dollarText)*usd_to_myr_rate;
     }
     public Float getResult()
     {
